e51bf2b25b31dcc01475fc430ce0310b390dd68b
Trickkyyy <vandark5@gmail.com>
2025-03-12 23:37:35 +0200

    Removing unused files

--- src/hooks/useRecipes.ts
+++
@@ -1,8 +0,0 @@
-import { useMutation } from "@tanstack/react-query";
-import { RecipeResponse, recipeService } from "@/api/recipeServices";
-
-export const useRecipes = () => {
-  return useMutation<RecipeResponse, Error, Record<string, string | string[]>>({
-    mutationFn: (answer) => recipeService.getBestRecipes(answer),
-  });
-};
