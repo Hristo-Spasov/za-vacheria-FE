cacb60d0928387cd975e3e86a9e6b65cef351990
unknown <vandark5@gmail.com>
2025-06-24 14:59:40 +0300

    adding a rate limit function

---
+++ src/lib/rateLimiter.ts
@@ -0,0 +1,28 @@
+import redis from "@/lib/clients/redis";
+
+const rateLimiter = async (
+  scope: string,
+  ip: string,
+  limit: number = 2,
+  time: number = 60
+) => {
+  const key = `rate-limit:${scope}:${ip}`;
+  const now = Date.now();
+  const removeTime = now - time * 1000;
+
+  await redis.lpush(key, now.toString());
+  await redis.ltrim(key, 0, limit - 1);
+
+  const timestamps = await redis.lrange(key, 0, -1);
+  const filtered = timestamps.filter((ts) => parseInt(ts) > removeTime);
+
+  if (filtered.length >= limit) {
+    throw new Error(`Too many reports.Try again later.`);
+  }
+
+  await redis.expire(key, time);
+
+  return true;
+};
+
+export default rateLimiter;
